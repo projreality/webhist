3f24d53f27d89ae543f4865c85cbf5c36c9d558f
Samuel Li <sam@projreality.com>
2019-03-14 22:55:53 -0700

    Update README and version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = fd.read();
 fd.close();
 
 setup(name              = "webhist",
-      version           = "0.1.0",
+      version           = "1.0.0",
       packages          = find_packages(),
       author            = "Samuel Li",
       author_email      = "sli@projreality.com",
